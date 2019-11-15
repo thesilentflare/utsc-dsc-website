7c0879d24bce519f0c4a5526ee90a217d223571d
Jason Tian <tianjason6@gmail.com>
2019-11-15 16:09:12 -0500

    Created PastEvent Component

---
+++ src/Components/PastEvents/PastEvents.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBCollapse,
+  MDBJumbotron,
+  MDBIframe,
+  MDBNavbarToggler
+} from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import styles from "./PastEvents.module.css";
+
+class PastEvents extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <Router>
+            <MDBNavbar color="white" light expand="md">
+              <MDBNavbarBrand className="logo">
+                {/*Change logo to your logo placed in public folder*/}
+                <a href="/">
+                  <img
+                    className="navbar-brand"
+                    href="/"
+                    expand="lg"
+                    src={process.env.REACT_APP_navbar_logo}
+                    height="80"
+                    width="145"
+                    alt=""
+                  />
+                </a>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.toggleCollapse} />
+              <MDBCollapse
+                id="navbarCollapse3"
+                isOpen={this.state.isOpen}
+                navbar
+              >
+                <MDBNavbarNav right>
+                  {/*Change/Add navbar links here*/}
+                  <MDBNavItem className="links">
+                    <MDBNavLink to="/pastEvents">Past Events</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem className="links">
+                    <MDBNavLink to="/">Events</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem className="links">
+                    <MDBNavLink to="/">About</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem className="links">
+                    <MDBNavLink to="/">Calendar</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem className="links">
+                    <MDBNavLink to="/">Contact</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/">
+                      <Button variant="contained" color="primary">
+                        Subscribe
+                      </Button>
+                    </MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </Router>
+        </div>
+        <div>
+          <h1>Jason was here.</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PastEvents;
--- src/Main/App.js
+++ src/Main/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from './Landing.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import Landing from "./Landing.js";
+import pastEvents from "../Components/PastEvents/PastEvents.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={Landing}/>
-          { /* Finally, catch all unmatched routes */ }
-        { /*  <Route component={NotFound} />*/ }
+          <Route path="/pastEvents" exact component={pastEvents} />
+          <Route path="/" exact component={Landing} />
+          {/* Finally, catch all unmatched routes */}
+          {/*  <Route component={NotFound} />*/}
         </Switch>
       </Router>
     </div>
